06f8e3153a9dadeb46d9deec5de5bd3503b51192
Igor Moreno <35140312+MorenoIgor@users.noreply.github.com>
2024-05-17 08:54:27 -0300

    Update meucodigo.js

--- truejuan/meucodigo.js
+++ truejuan/meucodigo.js
@@ -2,8 +2,6 @@ var sequencia = [1,2,3,4]
 //A sequência é de dois em dois
 var opcao_correta = 2
 
-var btnc = document.querySelector("#botao3")
-btnc.addEventListener("click", verificarResposta)
 var trocar = false
 var butao = document.querySelector("#botao")
 
